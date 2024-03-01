457dd636b9e8d825d6b18211f48d1b3c622ca4c2
Pablo Santos <opablosantos@gmail.com>
2024-03-01 18:10:47 -0300

    refactor(ap05): fim da seção 2.4
    - Instalação do Bootstrap
    - Instalação do FontAwesome
    - Aplicação inicial refatorada
    - Componente App definido por meio de uma classe

---
+++ estacao-climatica/src/index.js
@@ -0,0 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import ReactDOM from "react-dom"
+import React from "react"
+
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+                Meu App
+            </div>
+        )
+    }
+}
+
+// export default function App() {
+//     window.navigator.geolocation.getCurrentPosition(
+//         position => console.log(position)
+//     )
+//     return (
+//         <div>
+//             Meu App
+//         </div>
+//     )
+// }
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+)
