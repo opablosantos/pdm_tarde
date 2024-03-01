67b67cd08b0806c0bd93c7bb753626154f291d6e
Pablo Santos <opablosantos@gmail.com>
2024-03-01 16:28:31 -0300

    refactor(ap04): fim da seção 2.8
    - Componente Cartao
    - Pedido deixa de utilizar classes de cartão do Bootstrap
    - Props de Pedido desestruturados

--- react-componentes-props/src/index.js
+++ react-componentes-props/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import Pedido from "./Pedido";
+import Cartao from "./Cartao";
 
 const App = () => {
     return (
@@ -15,13 +16,19 @@ const App = () => {
 
             <div className="row">
                 <div className="col-sm-12 col-lg-4">
-                    <Pedido data="12/03/2024" icone="fa-solid fa-mobile-screen-button fa-2x" titulo="iPhone 4" descricao="64GB"/>
+                    <Cartao cabecalho="14/03/2024">
+                        <Pedido icone="fa-solid fa-mobile-screen-button fa-2x" titulo="iPhone 4" descricao="64GB"/>
+                    </Cartao>
                 </div>
                 <div className="col-sm-12 col-lg-4">
-                    <Pedido data="13/03/2024" icone="fa-solid fa-book fa-2x" titulo="Livro" descricao="React from the scratch"/>
+                    <Cartao cabecalho="13/03/2024">
+                        <Pedido icone="fa-solid fa-book fa-2x" titulo="Livro" descricao="React from the scratch"/>
+                    </Cartao>
                 </div>
                 <div className="col-sm-12 col-lg-4">
-                    <Pedido data="14/03/2024" icone="fa-solid fa-laptop fa-2x" titulo="Notebook" descricao="Windows i5 8GB 1TB"/>
+                    <Cartao cabecalho="14/03/2024">
+                        <Pedido icone="fa-solid fa-laptop fa-2x" titulo="Notebook" descricao="Windows i5 8GB 1TB"/>
+                    </Cartao>
                 </div>
             </div>         
         </div>
